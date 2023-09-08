7b8e8ece8c025ff28be8898c80897e62e5087e96
Pamela Glickman <pamela@Pamelas-MacBook-Pro.local>
2023-09-08 21:50:33 +1000

    Added app functionality

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const guessResult = document.querySelector("#guessResult");
+
 // Show/Hide Menu
 
 const menubarMenu = document.querySelector(".menubar__start-menu");
@@ -20,6 +22,12 @@ appButtons.forEach((button) => {
       modal.classList.remove("main__modal--show");
     });
     const targetId = event.currentTarget.getAttribute("value");
+    if (targetId === "greetings") {
+      showGreeting();
+    } else if (targetId === "numberGuess") {
+      let correctNumber = Math.floor(Math.random() * 10);
+      setUpNumberGuess(correctNumber);
+    }
     const targetModal = document.querySelector(`#${targetId}`);
     targetModal.classList.add("main__modal--show");
   });
@@ -28,6 +36,9 @@ appButtons.forEach((button) => {
 closeButtons.forEach((button) => {
   button.addEventListener("click", (event) => {
     const targetId = event.currentTarget.getAttribute("value");
+    if (targetId === "numberGuess") {
+      guessResult.textContent = "";
+    }
     const targetModal = document.querySelector(`#${targetId}`);
     targetModal.classList.remove("main__modal--show");
   });
@@ -46,3 +57,42 @@ setInterval(() => {
   const timeStr = `${hours}:${minutes} ${amOrPM}`;
   clock.textContent = timeStr;
 }, 1000);
+
+// Greetings App
+
+const greetingsText = document.querySelector("#greetingsText");
+
+const showGreeting = () => {
+  let randomNum = Math.random();
+  let greeting = "";
+
+  if (randomNum < 0.25) {
+    greeting = "Hello There!";
+  } else if (randomNum < 0.5) {
+    greeting = "Bonjour!";
+  } else if (randomNum < 0.75) {
+    greeting = "Hola!";
+  } else {
+    greeting = "Hey, what's up?";
+  }
+
+  greetingsText.textContent = greeting;
+};
+
+// Number Guess App
+
+const numberInput = document.querySelector("#numberInput");
+const numberSubmit = document.querySelector("#numberSubmit");
+
+const setUpNumberGuess = (correctNumber) => {
+  numberSubmit.addEventListener("click", (event) => {
+    event.preventDefault();
+    let guess = Number(numberInput.value);
+
+    if (guess === correctNumber) {
+      guessResult.textContent = "Correct!";
+    } else {
+      guessResult.textContent = "Keep guessing!";
+    }
+  });
+};
