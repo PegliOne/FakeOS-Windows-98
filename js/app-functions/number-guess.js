15cb37d747cba6161ed6a351192f0d751975c639
Pamela Glickman <pamela@Pamelas-MacBook-Pro.local>
2023-09-19 17:33:18 +1000

    Added number guessing logic and styling

--- js/app-functions/number-guess.js
+++ js/app-functions/number-guess.js
@@ -1,6 +1,6 @@
 export const setUpNumberGuess = () => {
   const numberSubmit = document.querySelector("#numberSubmit");
-  let correctNumber = Math.floor(Math.random() * 10);
+  let correctNumber = Math.floor(Math.random() * 100 + 1);
 
   numberSubmit.addEventListener("click", (event) => {
     event.preventDefault();
@@ -12,10 +12,17 @@ const checkGuess = (correctNumber) => {
   const numberInput = document.querySelector("#numberInput");
   let guess = Number(numberInput.value);
 
+  guessResult.classList.remove("modal__para--success", "modal__para--danger");
+
   if (guess === correctNumber) {
     guessResult.textContent = "Correct!";
+    guessResult.classList.add("modal__para--success");
+  } else if (guess > correctNumber) {
+    guessResult.textContent = "Too high!";
+    guessResult.classList.add("modal__para--danger");
   } else {
-    guessResult.textContent = "Keep guessing!";
+    guessResult.textContent = "Too low!";
+    guessResult.classList.add("modal__para--danger");
   }
 };
 
